baeac20c6f710031173a1769285fa8122036465f
Liam <78303756+Lduignan1@users.noreply.github.com>
2022-06-10 00:08:47 +0200

    Update Assignment_2.py

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 NAME = input("Hi, what is your name? ")
 
 cm = int(input("\nWhat is your height in centimetres? "))
@@ -11,17 +10,4 @@ Q = int(Q)
 
 print("\nDear",NAME.upper(),"you are approximately",F,"feet",I, str(Q) + "/4 inches tall")
 input("\nPress the enter key to exit")
-=======
-NAME = input("Hi, what is your name? ")
 
-cm = int(input("\nWhat is your height in centimetres? "))
-F = cm * 0.0328084
-I = (F - int(F)) * 12
-Q = (I - int(I)) * 4
-F = int(F)
-I = int(I)
-Q = int(Q)
-
-print("\nDear",NAME.upper(),"you are approximately",F,"feet",I, str(Q) + "/4 inches tall")
-input("\nPress the enter key to exit")
->>>>>>> faa940edcaa09727429bef07f5640088a06e1390

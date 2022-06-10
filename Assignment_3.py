51a9c7e0b1700fa3fe1ed199ff7ce12611ac1358
Liam <duignan.liam202@gmail.com>
2022-06-10 09:26:23 +0200

    added a funny condition to Assignment_3.py

--- Assignment_3.py
+++ Assignment_3.py
@@ -5,5 +5,9 @@ F = cm * 0.0328084
 I = (F - int(F)) * 12
 Q = (I - int(I)) * 4
 
+if (F < 5):
+    print("You're still a wee lad")
+
 print("\nDear",NAME.upper(),"you are approximately",F,"feet",I, str(Q) + "/4 inches tall")
+
 input("\nPress the enter key to exit")

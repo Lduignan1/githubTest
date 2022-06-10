370176f625a39c66cb7c34c880246d746700ae56
Liam <duignan.liam202@gmail.com>
2022-06-10 09:16:55 +0200

    Added Assignment_3.py

---
+++ Assignment_3.py
@@ -0,0 +1,9 @@
+NAME = input("Hi, what is your name? ")
+
+cm = int(input("\nWhat is your height in centimetres? "))
+F = cm * 0.0328084
+I = (F - int(F)) * 12
+Q = (I - int(I)) * 4
+
+print("\nDear",NAME.upper(),"you are approximately",F,"feet",I, str(Q) + "/4 inches tall")
+input("\nPress the enter key to exit")
